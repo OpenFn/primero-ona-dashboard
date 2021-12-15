aa9fc2cf2e4bf2908840626736734ced6b94b6ea
daissatou2 <80456839+daissatou2@users.noreply.github.com>
2021-12-15 04:50:43 -0600

    aissatou@openfn.org's change on OpenFn.org

--- jobs/upsertCasesToONA.js
+++ jobs/upsertCasesToONA.js
@@ -1,1 +1,14 @@
-// Your job goes here.
+each(
+  'cases[*]',
+  upsert('cases', 'case_id', c => ({
+    case_id: c.case_id_display,
+    registration_date: c.registration_date,
+    case_source: c.oscar_number || 'primero',
+    disabled: c.disability_type,
+    sex: c.sex,
+    age: c.age,
+    placement_type: c.type_of_placement,
+    province: c.location_caregiver,
+    district: c.location_caregiver,
+  }))
+);
