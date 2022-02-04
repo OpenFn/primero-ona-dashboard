e20ebdbfd311e13caa2f308451d360a7d48380ea
daissatou2 <80456839+daissatou2@users.noreply.github.com>
2022-02-04 17:37:37 +0000

    elias@openfn.org's change on OpenFn.org

--- jobs/upsertCasesToONA.js
+++ jobs/upsertCasesToONA.js
@@ -10,7 +10,7 @@ each(
     protection_concern:
       c.data.protection_concerns && c.data.protection_concerns.join(','),
     
-    placement_type: c => c.data.placement_type && c.data.placement_type.split("_").slice(0, -1).join(" ");
+    placement_type: c => c.data.placement_type && c.data.placement_type.split("_").slice(0, -1).join(" ")
     // province: c.data.location_caregiver || c.data.location_current,
     // district: c.data.location_caregiver || c.data.location_current,
   }))
