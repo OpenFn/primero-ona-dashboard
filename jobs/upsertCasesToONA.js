59b6fa4a98f6fa352b0ed2125d0608ec03889661
aleksa-krolls <aleksagk@gmail.com>
2022-02-07 15:23:48 +0200

    add mapping placeholder

--- jobs/upsertCasesToONA.js
+++ jobs/upsertCasesToONA.js
@@ -1,3 +1,14 @@
+fn(state => {
+  //Mapping table for protection concerns
+  const protectionMap = {
+    disabled: 'At risk of neglect',
+    serious_health_issue: 'Experiencing neglect',
+    //TODO: Add other mapping values from XLS
+  };
+
+  return { ...state, protectionMap };
+});
+
 each(
   'cases[*]',
   upsert('cases', 'case_id', c => ({
@@ -9,23 +20,26 @@ each(
     age: c.data.age,
     protection_concerns:
       c.data.protection_concerns && c.data.protection_concerns.join(','),
-    
-    placement_type: c => c.data.placement_type && c.data.placement_type.split("_").slice(0, -1).join(" "),
+    //We expect that protection_concerns might have multiple values separated by a space (e.g., 'disabled serious_health_issue')
+    //TODO: For each protection_concerns value, check protectionMap for the new value to relabel, and then join by ','
+    //Example transformation: 'disabled serious_health_issue' --> 'At risk of neglect, Experiencing neglect'
+    placement_type: c =>
+      c.data.placement_type &&
+      c.data.placement_type.split('_').slice(0, -1).join(' '),
     district_current: await findValue({
       uuid: 'district',
       relation: 'locations',
       where: {
-        location_code: dataValue('location_current')(state)
-        
-      }
-    })(state)
+        location_code: dataValue('location_current')(state),
+      },
+    })(state),
     // province: c.data.location_caregiver || c.data.location_current,
     // district: c.data.location_caregiver || c.data.location_current,
   }))
 );
 
 fn(state => {
-  console.log("PLACEMENT");
+  console.log('PLACEMENT');
   console.log(state.data.placement_type);
   return state;
 });
@@ -40,7 +54,10 @@ fn(state => {
           case_id: case_id,
           unique_id: service.unique_id,
           service_type: service.service_type,
-          service_date: service.service_implemented_day_time === 'not implemented' ? null : service.service_implemented_day_time,
+          service_date:
+            service.service_implemented_day_time === 'not implemented'
+              ? null
+              : service.service_implemented_day_time,
         }));
       return services;
     })
