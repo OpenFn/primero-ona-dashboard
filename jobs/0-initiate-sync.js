02ca5ed4f9299eb89cd59dab54c24679d62c0a1d
mtuchi <mtuchidev@gmail.com>
2022-10-25 15:57:18 +0300

    feat: add trigger value

--- jobs/0-initiate-sync.js
+++ jobs/0-initiate-sync.js
@@ -1,6 +1,7 @@
 // post to openfnInboxUrl initialPayload
 post(`${state.configuration.inboxUrl}`, {
   body: {
+    trigger: 'Job 0/3 Succeeds',
     metadataForTypeofCaseRequest: {
       per: 500,
       page: 1,
