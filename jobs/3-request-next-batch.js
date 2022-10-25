90061e6d3260abcbdf695135aa8bc9c2f59c73ab
mtuchi <mtuchidev@gmail.com>
2022-10-25 15:38:04 +0300

    feat: add job that will update inbox for the next round

---
+++ jobs/3-request-next-batch.js
@@ -0,0 +1,37 @@
+// post to openfnInboxUrl the next round payload
+fn(state => {
+  const { metadataForAgeRequest, metadataForTypeofCaseRequest } = state;
+  // decide if next round is important
+  const pageNextRoundDecision = metadata => {
+    const { total, per, page } = metadata;
+
+    const pageDataRequested = per * page;
+
+    if (total > pageDataRequested) {
+      metadata['page'] = metadata['page'] + 1;
+      return metadata;
+    }
+
+    return { ...metadata, getcases: false };
+  };
+
+  const pageNextRoundPayload = {
+    metadataForAgeRequest: pageNextRoundDecision(metadataForAgeRequest),
+    metadataForTypeofCaseRequest: pageNextRoundDecision(
+      metadataForTypeofCaseRequest
+    ),
+  };
+
+  console.log(
+    'Next round of getCases metadata',
+    JSON.stringify(pageNextRoundPayload, null, 2)
+  );
+
+  return { ...state, pageNextRoundPayload };
+});
+
+post(`${state.configuration.inboxUrl}`, {
+  body: state => {
+    return state.pageNextRoundPayload;
+  },
+});
