69c624269a26acc18b8b77d0c5b80e567f480b63
mtuchi <mtuchidev@gmail.com>
2022-10-25 15:58:39 +0300

    feat: update initial payload

--- jobs/0-initiate-sync.js
+++ jobs/0-initiate-sync.js
@@ -1,24 +1,13 @@
-
-// Setup initial payload for initialState
-// fn(state => {
-//   if (!state.data) {
-//     const initialPayload = {
-//       trigger: 'Job 0/3 Succeeds',
-//       per: 500,
-//       ageLastRun: 1,
-//       typeOfCaseLastRun: 1,
-//     };
-//     state.data = initialPayload;
-//     return state;
-//   }
-//   return state;
-// });
-
 // post to openfnInboxUrl initialPayload
 post(`${state.configuration.inboxUrl}`, {
   body: {
-    per: 500,
-    ageLastRun: 1,
-    typeOfCaseLastRun: 1,
+    metadataForTypeofCaseRequest: {
+      per: 500,
+      page: 1,
+    },
+    metadataForAgeRequest: {
+      per: 500,
+      page: 1,
+    },
   },
 });
