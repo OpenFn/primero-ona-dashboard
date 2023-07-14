b3038146d9f8c65165599de8d91f4bfed81b322d
daissatou2 <80456839+daissatou2@users.noreply.github.com>
2023-07-14 10:10:35 -0500

    aissatou@openfn.org's change on OpenFn.org

--- jobs/getCasesFromPrimero.js
+++ jobs/getCasesFromPrimero.js
@@ -1,27 +1,114 @@
-// post to openfnInboxUrl pageNextRoundPayload
 fn(state => {
-  const initialMeta = {
-    per: 500,
-    page: 1,
-    getcases: true,
+  const dateCursor = '2023-01-01T00:00:00.000Z..2023-12-31T23:59:00.000Z'
+  const { metadataForAgeRequest, metadataForTypeofCaseRequest } = state.data;
+
+  console.log('pageForAgeRequest:', metadataForAgeRequest.page);
+  console.log('pageForTypeOfCaseRequest:', metadataForTypeofCaseRequest.page);
+
+  return {
+    ...state,
+    metadataForAgeRequest,
+    metadataForTypeofCaseRequest,
+    dateCursor
   };
-
-  const pageNextRoundPayload = {
-    trigger: 'Job 0/3 Succeeds',
-    metadataForAgeRequest: initialMeta,
-    metadataForTypeofCaseRequest: initialMeta,
-  };
-
-  console.log(
-    'Next round of getCases metadata',
-    JSON.stringify(pageNextRoundPayload, null, 2)
-  );
-
-  return { ...state, pageNextRoundPayload };
 });
 
-post(`${state.configuration.inboxUrl}`, {
-  body: state => {
-    return state.pageNextRoundPayload;
-  },
+fn(state => {
+  const { metadataForTypeofCaseRequest } = state;
+
+  if (!metadataForTypeofCaseRequest.getcases) return state;
+
+  return getCases(
+    state => 
+    ({
+      record_state: true,
+      type_of_case: 'children_undergoing_reintegration_55427',
+      created_at: state.dateCursor,
+      per: 500,
+      page: metadataForTypeofCaseRequest.page,
+    }),
+    null,
+    state => {
+      const { data, metadata } = state;
+      metadataForTypeofCaseRequest['total'] = metadata.total;
+      metadataForTypeofCaseRequest['page'] = metadata.page;
+      metadataForTypeofCaseRequest['per'] = metadata.per;
+
+      return {
+        ...state,
+        cases: data,
+        metadataForTypeofCaseRequest,
+      };
+    }
+  )(state);
+});
+
+fn(state => {
+  const { metadataForAgeRequest } = state;
+
+  if (!metadataForAgeRequest.getcases) return state;
+  return getCases(
+    state =>
+    ({
+      record_state: true,
+      created_at: state.dateCursor,
+      age: '0..18',
+      per: 500,
+      page: metadataForAgeRequest.page,
+    }),
+    null,
+    state => {
+      const { data, metadata } = state;
+      const typeOfCaseCases = state.cases ? state.cases : [];
+      const allCases = [...typeOfCaseCases, ...data];
+
+      console.log('allCases', allCases.length);
+
+      const casesUIDS = allCases.map(c => c.case_id_display);
+
+      const findDuplicates = arr =>
+        arr.filter((item, index) => arr.indexOf(item) != index);
+
+      const casesDuplicateUIDS = [...new Set(findDuplicates(casesUIDS))];
+
+      const casesWithDuplicates = casesDuplicateUIDS.map(uid => {
+        if (uid.length === 0) {
+          console.log('case_id_display is empty');
+        } else {
+          console.log(uid, 'case_id_display has duplicates');
+        }
+        return allCases.filter(c => c.case_id_display === uid);
+      });
+      console.log(
+        'cases with the same case_id_display: ',
+        JSON.stringify(casesWithDuplicates, null, 4)
+      );
+
+      const deDuplicatedCases = Array.from(
+        new Set(allCases.map(s => s.case_id_display))
+      )
+        .map(id => {
+          return allCases.find(s => s.case_id_display === id);
+        })
+        .flat();
+
+      // console.log(
+      //   'deDuplicatedCases',
+      //   JSON.stringify(deDuplicatedCases, null, 4)
+      // );
+
+      console.log('deDuplicatedCases', deDuplicatedCases.length);
+      metadataForAgeRequest['total'] = metadata.total;
+      metadataForAgeRequest['page'] = metadata.page;
+      metadataForAgeRequest['per'] = metadata.per;
+
+      return {
+        ...state,
+        cases: deDuplicatedCases,
+        metadataForAgeRequest,
+        casesWithDuplicates,
+        references: [],
+      };
+    }
+  )(state);
 });
