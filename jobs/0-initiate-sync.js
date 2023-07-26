c13e19e15f664f1b30b249f3d82b3e0295090f1b
daissatou2 <80456839+daissatou2@users.noreply.github.com>
2023-07-26 07:47:22 -0500

    mtuchi@openfn.org's change on OpenFn.org

--- jobs/0-initiate-sync.js
+++ jobs/0-initiate-sync.js
@@ -5,7 +5,7 @@ fn(state => {
     page: 1,
     getcases: true,
   };
-  
+  console.log(state.metadataForAgeRequest, 'metadataForAgeRequest')
   const runStartedAt = new Date().toISOString();
   const manualCursor = `2023-03-26T06:55:31.494Z`;
   
