3df523aedac322e4ef9b394446167be5966219a6
daissatou2 <80456839+daissatou2@users.noreply.github.com>
2023-07-26 07:12:42 -0500

    mtuchi@openfn.org's change on OpenFn.org

--- jobs/0-initiate-sync.js
+++ jobs/0-initiate-sync.js
@@ -8,10 +8,10 @@ fn(state => {
   
   const runStartedAt = new Date().toISOString();
   const manualCursor = `2023-03-26T06:55:31.494Z`;
+  
+  console.log(typeof state.lastRunDateTime)
 
-  const dateCursor = state.lastRunDateTime
-    ? `${state.lastRunDateTime}..${runStartedAt}`
-    : `${manualCursor}..${runStartedAt}`;
+  const dateCursor = state.lastRunDateTime ? `${state.lastRunDateTime}..${runStartedAt}` : `${manualCursor}..${runStartedAt}`;
 
 console.log('lastRunDateTime', state.lastRunDateTime)
 
